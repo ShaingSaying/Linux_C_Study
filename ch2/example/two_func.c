e969c2653b382145f92cc327c57be5233ff042c9
opfanswyx <opfanswyx@outlook.com>
2017-04-25 10:45:53 +0800

    ch2 exercises

---
+++ ch2/example/two_func.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void butler();
+int main()
+{
+	printf("i will summon the butler function.\n");
+	butler();
+	printf("yes.bring me some tea and writeable DVDs.\n");
+	
+	return 0;
+}
+void butler()
+{
+	printf("you rang,sir?\n");
+}
